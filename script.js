a4b03fdd4c2c3137273637792932c46ff28d5d5c
Deniz Pandar <denizpandar@gmail.com>
2025-07-05 00:18:09 +0100

    Add buttons on javascript and add button id's on html

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-const startButton = document.getElementById('start-button')
+let startButton;
+
+document.addEventListener("DOMContentLoaded", () => {
+startButton = document.getElementById('startButton') // get const from a tutorial
+const nextButton = document.getElementById('nextButton')
+const rateButton = document.getElementById('rateButton')
 
 startButton.addEventListener('click', startQuiz)
-
+nextButton.addEventListener("click", nextQuestion);
+rateButton.addEventListener("click", rateGame);
+});
 
 function startQuiz() {
     const username = document.getElementById("username").value;
 
+    console.log("Username:", username)
     if (!username.trim()) {
         alert("Please enter a username!");
         return;
     }
 
+    console.log("Starting");
+    startButton.classList.add("hidden")
+    
     document.getElementById("usernameScreen").classList.add("hidden");
-    document.getElementById("quizScreen").classList.remove("hidden");
+    document.getElementById("question-container").classList.remove("hidden");
 
     showQuestion();
 }
@@ -25,5 +36,9 @@ function nextQuestion() {
 }
 
 function checkAnswer() {
+
+}
+
+function rateGame(){
 
 }
